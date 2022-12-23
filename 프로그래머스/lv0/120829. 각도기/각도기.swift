228a91bd567d065692da76d7fb5f0abefef72d30
hwangyeri <114602459+hwangyeri@users.noreply.github.com>
2022-12-24 01:40:40 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    var result: Int = 0
+    
+    if 0 < angle && angle < 90 {
+        result = 1
+    } else if angle == 90 {
+        result = 2
+    } else if 90 < angle && angle < 180 {
+        result = 3
+    } else if angle == 180 {
+        result = 4
+    }
+    
+    return result
+}
