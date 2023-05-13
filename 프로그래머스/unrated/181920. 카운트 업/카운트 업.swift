9c5d7d79f8560c0da143ccbfa51d367247c1bac8
Yeri Hwang <114602459+hwangyeri@users.noreply.github.com>
2023-05-13 17:23:35 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ start:Int, _ end:Int) -> [Int] {
+    var result = [Int]()
+    for number in start...end {
+        result.append(number)
+    }
+    return result
+}
