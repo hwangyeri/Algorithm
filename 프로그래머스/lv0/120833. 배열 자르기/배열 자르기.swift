99057d4e1839a6a05ee1b583b66c6794faf0730d
Yeri Hwang <114602459+hwangyeri@users.noreply.github.com>
2023-08-24 10:53:52 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.swift
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.swift
@@ -3,5 +3,5 @@ import Foundation
 func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
     var result = numbers[num1...num2] // type - ArraySlice<Int>
     
-    return Array(result) // Array[Int]로 리턴 받을수있게 형변환 해주기
+    return Array(result) // Array[Int]로 리턴 받을 수 있게 형변환 해주기
 }
