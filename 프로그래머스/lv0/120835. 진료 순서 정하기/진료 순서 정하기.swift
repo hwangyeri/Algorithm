7d4f4ce08fd2eb7e831ca39a731676090c330d48
Yeri Hwang <114602459+hwangyeri@users.noreply.github.com>
2023-08-28 20:00:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.10 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.swift
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 func solution(_ emergency:[Int]) -> [Int] {
-    return []
+    var newArr = emergency.sorted(by: >) // 큰수부터 내림차순 정렬
+    var result = [Int]()
+    for one in 0..<newArr.count { // 두 배열을 비교해서
+        for two in 0..<newArr.count {
+            if emergency[one] == newArr[two] { // 똑같은 값을 찾아서
+                result.append(two + 1) // 해당 인덱스 값 저장, 진료순서는 1부터 시작
+            }
+        }
+    }
+    return result
 }
