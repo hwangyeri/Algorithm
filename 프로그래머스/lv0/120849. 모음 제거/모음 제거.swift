410c07ae9482ad4be2fe27c5eb5822e619daad60
Yeri Hwang <114602459+hwangyeri@users.noreply.github.com>
2023-09-18 13:33:50 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.swift
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
+    // 쪼개면서 모음 제거됨, 나머지 문자열로 합치기
     return my_string.components(separatedBy: ["a", "e", "i", "o", "u"]).joined()
 }
